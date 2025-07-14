f79123939fda8f464586d0abc9620dd842930862
Pallavi Sastry <89313238+pallavi-sastry@users.noreply.github.com>
2025-07-15 00:31:52 +0530

    Created basic react element

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createElement } from 'react'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'))
+
+const reactElement = <h1>Hello from JSX!</h1>
+
+console.log(reactElement)
+
+root.render(
+  reactElement
 )
