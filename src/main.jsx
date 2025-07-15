8c441bbe149361bddf7d35f8e905396fc236919e
Pallavi Sastry <89313238+pallavi-sastry@users.noreply.github.com>
2025-07-15 21:41:41 +0530

    Created components

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,8 @@ import App from './App.jsx'
 
 const root = createRoot(document.getElementById('root'))
 
-const reactElement = <h1>Hello from JSX!</h1>
-
-console.log(reactElement)
-
 root.render(
-  reactElement
+  <div>
+    <App />
+  </div>
 )
